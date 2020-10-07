0da36c4fb1f302ed0d0879d062fb6c66a02ff909
peterchave <pchave77@gmail.com>
2020-10-07 09:16:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
+# Add repos to get the latest version of ffmpeg, standard Ubuntu version is too old to support LL-DASH
 sudo add-apt-repository -y ppa:savoury1/ffmpeg4
 sudo add-apt-repository -y ppa:savoury1/graphics
 sudo add-apt-repository -y ppa:savoury1/multimedia
 sudo apt-get update -qq 
-sudo apt-get -y install ffmpeg
+sudo apt-get -y install ffmpeg nginx libnginx-mod-rtmp
 
-# Download test clip
-cd ..
+
+# OPTIONAL: Download test clip
 wget https://moctodemo.akamaized.net/content/testclip.mp4
 
 # Install PM2 to run it, save the config, make it start at boot and then shut it down for further config
 sudo apt-get -y install npm
 sudo npm install pm2@latest -g
+
+# inject EntryPoint into encoder script
+echo http://ep$1.i.akamaientrypoint.net/$1/event/out.mpd >> script.sh
 chmod +x script.sh
 pm2 start script.sh
 pm2 save
@@ -19,4 +23,3 @@ chmod +x startup.sh
 ./startup.sh
 rm startup.sh
 pm2 stop script.sh
-echo "TO FINISH: Modify script.sh to add entrypoint URL and then restart script with pm2 start script"

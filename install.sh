0d203ed9c073f800c638f0e0f41a142aeefd4ce7
peterchave <pchave77@gmail.com>
2020-10-12 14:28:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ sudo add-apt-repository -y ppa:savoury1/multimedia
 sudo apt-get update -qq 
 sudo apt-get -y install ffmpeg nginx libnginx-mod-rtmp
 
-# OPTIONAL: Download test clip
-# wget https://moctodemo.akamaized.net/content/testclip.mp4
-
 # Install PM2 to run it, save the config, make it start at boot and then shut it down for further config
 sudo apt-get -y install npm
 sudo npm install pm2@latest -g

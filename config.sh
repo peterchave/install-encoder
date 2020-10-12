9d28e3c4ca3f5e23bc956e3e73f8821b660ee7a3
peterchave <pchave77@gmail.com>
2020-10-12 14:28:36 -0400

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+# config ngnix
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo service nginx restart
+
+# config EntryPoint into encoder script
+echo http://p-ep$1.i.akamaientrypoint.net/cmaf/$1/event/out.mpd >> script.sh
+chmod +x script.sh
+
+# enable pm2 to start on reboot, start script, save and then stop it.
+pm2 startup
+pm2 start script.sh
+pm2 save
+pm2 stop script
